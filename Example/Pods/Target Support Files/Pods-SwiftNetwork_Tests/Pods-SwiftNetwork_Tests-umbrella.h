87d8f85771002ac8d9d0ed54ac39aa51af77fc04
Henry Hou <henry.hou@intradiem.com>
2017-02-06 10:35:36 -0500

    final version

---
+++ Example/Pods/Target Support Files/Pods-SwiftNetwork_Tests/Pods-SwiftNetwork_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SwiftNetwork_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SwiftNetwork_TestsVersionString[];
+
